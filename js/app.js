4a6bc89827b544407acb2dbe4d34c10522e48da2
Thomas Horvath <horvthecloud@gmail.com>
2014-10-27 20:32:31 +0000

    Alpha 0.2
    Working fuzzy search :D

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-(function dBikes(){
+//(function dBikes(){
+	var stations;
+	var bigFuzz;
+	//INIT
 	var init = function(){
 		console.log("initialized");
-		getStation(25);
+		getStation(67);
+		console.log(requestUrl);
+		getStations();
 	};
 
 	var stationInfo = [];
@@ -18,12 +23,18 @@
 	//ON AJAX START
 	$(document).ajaxStart(function() {
 	 console.log("LOADING DATA");
+	 $('.loading').fadeIn();
 	});
 
 	//ON AJAX COMPLETE
 	$(document).ajaxComplete(function( event,request, settings ) {
 	  console.log("COMPLETED LOADING DATA");
 	  resetStation();
+	  if(stations){
+	  	stations = stations;
+	  }
+	  $('.loading').fadeOut();
+	  console.log(event);
 	});
 
 	//ON AJAX ERROR
@@ -59,7 +70,7 @@
 				freeBikes   : data.available_bikes
 			};
 			stationInfo.push(thisStation);
-			displayData();
+			displayData(stationInfo.length -1);
 		});
 	}
 
@@ -67,14 +78,47 @@
 	function resetStation(){
 		station = '';
 	}
-
+	// Array - Number,Name,Address,Latitude,Longtitude
+	var stations;
 	//PRINT TO DOM
-	function displayData(){
-		$('#data').append('Name : ' + stationInfo[0].name + '<br>');
-		$('#data').append('Total Stands : ' + stationInfo[0].totalStands + '<br>');
-		$('#data').append('Free Stands : ' + stationInfo[0].freeStands + '<br>');
-		$('#data').append('Free Bikes : ' + stationInfo[0].freeBikes );
+	function displayData(i){
+		$('#data').append('Name : ' + stationInfo[i].name + '<br>');
+		$('#data').append('Total Stands : ' + stationInfo[i].totalStands + '<br>');
+		$('#data').append('Free Stands : ' + stationInfo[i].freeStands + '<br>');
+		$('#data').append('Free Bikes : ' + stationInfo[i].freeBikes );
 	}
+	function getStations(){
+		requestUrl = 'dublin.json';
+		$.get(requestUrl, function(data){
+		 	stations = data;
+		});
+	}
+	var options = {
+	  keys: ['address'],   // keys to search in
+	}
+	function fuzzySearch(search){
+		var f = new Fuse(stations, options);
+		var fuzz = f.search(search);
+		domPrint(fuzz);
+	}
+
+	$('#search').bind('input', function() { 
+		fuzzySearch($(this).val());
+	});
+
+
+	function domPrint(results){
+		$('.result').html('<li id="first" data-station="'+ results[0].number +'">'+ results[0].name + '</li>' + '<li data-station="'+ results[1].number +'">' + results[1].name +'</li>');
+		enableClick();
+	}
+
+	function enableClick(){
+		$('li').click(function(){
+			var stationId = this.getAttribute('data-station')
+			getStation(stationId);
+		});
+	}
+
 	//RUN
 	init();
-})();
+//})();
